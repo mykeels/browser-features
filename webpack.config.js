9a7e2686220b9324ffe3cb9610b00d1cd9fc31e8
mykeels <mykehell123@gmail.com>
2020-05-14 11:38:44 +0100

    fix: build to env preset, using commonjs module loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,37 @@
 module.exports = {
-    entry: './index.js',
-    module: {
-      rules: [
-        {
-          test: /\.(js)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
+  mode: 'development',
+  entry: './index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                'env',
+                {
+                  modules: 'commonjs',
+                  useBuiltIns: 'usage'
+                }
+              ]
+            ]
+          }
         }
-      ]
-    },
-    resolve: {
-      extensions: ['*', '.js']
-    },
-    output: {
-      path: __dirname + '/dist',
-      publicPath: '/',
-      filename: 'index.js'
-    },
-    devServer: {
-      contentBase: './dist'
-    }
-  };
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'index.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
